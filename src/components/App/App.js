161a91807d938d8388d707d47bfa53e7263becd3
Hazieon <handrew3721@gmail.com>
2020-10-27 16:51:22 +0000

    tasks complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,9 +14,19 @@ function App() {
     <div className="App">
       <h1>JSX will go here!</h1>
       <Input handleChange={handleChange} />
-      <Item text={text} />
+      <Item text={text} font="Arial" />
+      <Item text={text} font="Comic Sans Ms" />
+      <Item text={text} font="Times New Roman" />
+      <Item text={text} font="Consolas" />
+      <Item text={text} font="Tahoma" />
     </div>
   );
 }
 
 export default App;
+
+
+// In App.js, duplicate the item component at least three times using your Item component. 
+//For each one, hand in font={}. In the {}, hand in a different font's name (just like the font names you'd use in your CSS normally) to each Item.
+
+// Now load up, type in a word, and bask in the glory of your React typeface extravaganza!
--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Item({ text }) {
-  return <li>{text}</li>;
+function Item({ text, font }) {
+  return <li style={{fontFamily: font}}>{text}</li>;
 }
 
 export default Item;
+
+
+
+
