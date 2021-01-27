6d4e6fe0482dc625683ebe43848fb2058560113a
Hazieon <handrew3721@gmail.com>
2021-01-27 15:50:55 +0000

    updated font functionality and css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,27 +6,28 @@ import Item from "../Item";
 function App() {
   const [text, setText] = useState("");
 
+  //our version: takes EVENT and takes fired event's value
   function handleChange(e) {
     setText(e.target.value);
   }
 
+  //before event is added:
+  // function handleChange(inputText) {
+  //   setText(inputText);
+  // }
   return (
     <div className="App">
-      <h1>JSX will go here!</h1>
+      <h1 style={{ fontFamily: "Comic Sans Ms" }}>Replace Comic Sans!</h1>
       <Input handleChange={handleChange} />
-      <Item text={text} font="Arial" />
-      <Item text={text} font="Comic Sans Ms" />
-      <Item text={text} font="Times New Roman" />
-      <Item text={text} font="Consolas" />
-      <Item text={text} font="Tahoma" />
+      <div className="fontSpace">
+        <h5>Comic Sans:</h5> <Item text={text} font="Comic Sans Ms" />
+        <h5>Arial:</h5> <Item text={text} font="Arial" />
+        <h5>Times New Roman:</h5> <Item text={text} font="Times New Roman" />
+        <h5>Consolas:</h5> <Item text={text} font="Consolas" />
+        <h5>Tahoma:</h5> <Item text={text} font="Tahoma" />
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
-// In App.js, duplicate the item component at least three times using your Item component. 
-//For each one, hand in font={}. In the {}, hand in a different font's name (just like the font names you'd use in your CSS normally) to each Item.
-
-// Now load up, type in a word, and bask in the glory of your React typeface extravaganza!
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Input({ handleChange }) {
-  return <input type="text" onInput={handleChange} />;
+  return (
+    <input
+      className="input"
+      type="text"
+      onInput={handleChange}
+      placeholder="Type here to test..."
+    />
+  );
 }
+//or instead of onInput= :
+//onChange, it will run this function:
+//onChange = ((event) => {handleChange(event.target.value)})
 
+//pass text from the EVENT into this
 export default Input;
